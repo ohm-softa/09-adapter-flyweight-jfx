3c2631a1c6bffc8d902e0ab3f400a45fe327a5ff
Jonathan Wilisch <wilischjo89084@th-nuernberg.de>
2023-05-29 14:10:37 +0200

    Flyweight implemented

--- src/main/java/ohm/softa/a09/model/FighterFactory.java
+++ src/main/java/ohm/softa/a09/model/FighterFactory.java
@@ -10,7 +10,11 @@ import ohm.softa.a09.resource.ResourceLoader;
 import ohm.softa.a09.util.NameGenerator;
 import javafx.scene.image.Image;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+
+import static org.apache.logging.log4j.ThreadContext.containsKey;
 
 /**
  * Factory to create new fighters
@@ -19,6 +23,8 @@ import java.util.Random;
  * @author Peter Kurfer
  */
 public final class FighterFactory {
+	// intrinsic fighterImage
+	// extrinsic pilot
 
 	private static final int NumberOfKnownFighterTypes = 6;
 	private final Random random;
@@ -31,6 +37,18 @@ public final class FighterFactory {
 		imageResourceLoader = new ResourceLoader<>(Image::new);
 	}
 
+	private HashMap<String, Image> images = new HashMap<>(); //Map for flyweight implementation
+	Image getImage(String path){
+		//if(images.containsKey(path))
+		//	return images.get(path);
+		//images.put(path, imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), path));
+		//return images.get(path);
+
+		if(!images.containsKey(path)){
+			images.put(path, imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), path));
+		}
+		return images.get(path);
+	}
 	/**
 	 * Create a random Fighter
 	 *
@@ -40,17 +58,23 @@ public final class FighterFactory {
 	public Fighter createFighter() {
 		switch (random.nextInt(NumberOfKnownFighterTypes)) {
 			case 0:
-				return new AWing(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/awing.jpg"));
+				//return new AWing(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/awing.jpg"));
+				return new AWing(nameGenerator.generateName(), getImage("fighter/awing.jpg"));
 			case 1:
-				return new XWing(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/xwing.jpg"));
+				//return new XWing(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/xwing.jpg"));
+				return new XWing(nameGenerator.generateName(), getImage("fighter/xwing.jpg"));
 			case 2:
-				return new YWing(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/ywing.jpg"));
+				//return new YWing(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/ywing.jpg"));
+				return new YWing(nameGenerator.generateName(), getImage("fighter/ywing.jpg"));
 			case 3:
-				return new TieBomber(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/tiebomber.jpg"));
+				//return new TieBomber(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/tiebomber.jpg"));
+				return new TieBomber(nameGenerator.generateName(), getImage("fighter/tiebomber.jpg"));
 			case 4:
-				return new TieFighter(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/tiefighter.jpg"));
+				//return new TieFighter(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/tiefighter.jpg"));
+				return new TieFighter(nameGenerator.generateName(), getImage("fighter/tiefighter.jpg"));
 			default:
-				return new TieInterceptor(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/tieinterceptor.jpg"));
+				//return new TieInterceptor(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/tieinterceptor.jpg"));
+				return new TieInterceptor(nameGenerator.generateName(), getImage(("fighter/tieinterceptor.jpg")));
 		}
 	}
 }
---
+++ src/main/java/ohm/softa/a09/resource/FxImageLoader.java
@@ -0,0 +1,11 @@
+package ohm.softa.a09.resource;
+
+import java.awt.*;
+
+public class FxImageLoader {
+	ResourceLoader<Image> resourceLoader = new ResourceLoader<>(Image::new);
+
+	public Image loadImage(String resourcePath){
+		return
+	}
+}
